0862299fab5e07fa3f4b24565d14eb8f064eb79d
Raphael Tang <raphael_tang@comcast.com>
2018-06-07 10:37:50 -0400

    Make GPU agnostic

--- mel_spectrogram.py
+++ mel_spectrogram.py
@@ -1,9 +1,10 @@
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
+import torch
 from torch.autograd import Variable
 import librosa
-from stft import STFT
+from .stft import STFT
 import os
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   
 os.environ["CUDA_VISIBLE_DEVICES"]="1"
@@ -25,7 +26,7 @@ class MelSpectrogram(torch.nn.Module):
         
         self.stft = STFT(filter_length=self.filter_length, hop_length=self.hop_length)
         mel_filters = librosa.filters.mel(self.sample_rate, self.filter_length, self.num_mels)
-        self.mel_filter_bank = Variable(torch.FloatTensor(mel_filters), requires_grad=False)       
+        self.mel_filter_bank = Variable(torch.FloatTensor(mel_filters).cuda(), requires_grad=False)
 
     def forward(self, input_data):
         magnitude, phase = self.stft.transform(input_data)
